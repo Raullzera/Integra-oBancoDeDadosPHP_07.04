be3248f9f763a7afeb1987061d697a885b768905
android <@gmail.com>
2026-04-07 12:30:36 -0300

    Integração com banco de dados funcionando

--- etimUsuario.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE  etimUsuario;
-
-USE etimUsuario;
-CREATE TABLE usuario(
-    id int PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR (255) not null,
-    email VARCHAR(255) not null unique,
-    senha VARCHAR (255) not null
-);
