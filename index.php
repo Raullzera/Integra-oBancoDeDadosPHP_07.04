e68d5f49f594cfc5082b07363f4bb9f7cae610bb
android <@gmail.com>
2026-04-07 12:38:52 -0300

    Integração com Banco de Dados Funcionando 07.03

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<form method="POST" action="login.php" >
+    <input type="text"     placeholder = "digite um nome "   name = "nome" >
+    <input type="text"     placeholder = "digite um email "  name = "email" >
+    <input type="password" placeholder = "digite uma senha "  name = "senha" >
+    <input type="submit" value= "Login"> <br>
+
+</form>
+
